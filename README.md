82f867605d306e0395d584aebdf0267ac89bd4fc
Jacob Joseph <jacob@dayflower.io>
2023-07-12 13:15:37 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Metabase Python with Notify API
+
+## Dependencies
+- [poetry](https://python-poetry.org/)
+- Must set [MB_API_KEY](https://www.metabase.com/docs/latest/configuring-metabase/environment-variables#mb_api_key) to use Notify API
+
+
+## Installation
+- `$ poetry install`
+- Copy `env.sample` to `.env` or set environment variables
+
+
+## Usage
+
+### Getting a Session Token (not required for Notify API)
+- requires environment variables
+    - METABASE_USER
+    - METABASE_PASS
+    - METABASE_URL
+
+`$ ./metabase-python-api-notify.py create_session_token`
+
+- or to store in environment variable
+
+`$ METABASE_SESSION_TOKEN=./metabase-python-api-notify.py create_session_token`
+
+### Listing databases
+- requires session token to be passed as an argument
+
+`$ ./metabase-python-api-notify.py $METABASE_SESSION_TOKEN`
+
+- response will be JSON list of databases and database attributes
+
+### Using Metabase Notify API
+- requires environment variables
+    - MB_API_KEY
+- requires argument
+    - database_id - should be integer
+- optional parameters
+    - table_id - should be integer (cannot use with table_name)
+    - table_name - should be string (cannot use with table_id)
+    - scan_type - should be string (either `full` or `schema`)
+
+- Format
+    - `$ ./metabase-python-api-notify.py database_id table_id=table_id table_name=table_name scan_type=scan_type`
+- Example: rescan database 15
+    - `$ ./metabase-python-api-notify.py 15`
+- Example: rescan database 15 table 41
+    - `$ ./metabase-python-api-notify.py 15 table_id=41`
+- Example: rescan database 15 table abc_123
+    - `$ ./metabase-python-api-notify.py 15 table_name=abc_123`
+- Example: rescan database 15 full
+    - `$ ./metabase-python-api-notify.py 15 scan_type=full`
+
---
+++ metabase-python-api-notify.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+import os
+import requests
+
+import fire
+
+from dotenv import load_dotenv
+
+# Load .env file if exists
+load_dotenv()
+
+
+def create_session_token():
+    """Creates a session token. Requires environment variables or .env file:
+        METABASE_USER, METABASE_PASS, and METABASE_URL.
+        """
+    metabase_url = os.getenv('METABASE_URL')
+
+    url = f'{metabase_url}/api/session'
+
+    response = requests.post(
+        url,
+        json={
+            'username': os.getenv('METABASE_USER'),
+            'password': os.getenv('METABASE_PASS'),
+        },
+    )
+
+    return response.json()['id']
+
+
+def list_databases(session_token):
+    """Calls GET /api/database/ to get a list of databases. Requires
+        session_token argument and environment variables or .env file for:
+        METABASE_USER, METABASE_PASS, and METABASE_URL.
+        """
+    metabase_url = os.getenv('METABASE_URL')
+
+    url = f'{metabase_url}/api/database/'
+
+    response = requests.get(
+        url,
+        headers={
+            'X-Metabase-Session': session_token,
+        },
+    )
+
+    return response.json()['data']
+
+
+# re: https://www.metabase.com/docs/latest/api/notify
+def notify_rescan_db(database_id, *args, table_id=None, table_name=None, scan_type=None):
+    """Notifies Metabase of potential schema change. Requires database_id
+        argument and MB_API_TOKEN environment variable. Optionally accepts
+        table_id, table_name, and scan_type.
+        """
+    metabase_url = os.getenv('METABASE_URL')
+
+    url = f'{metabase_url}/api/notify/db/{database_id}'
+
+    request_data = {}
+
+    if table_id:
+        request_data['table_id'] = table_id
+
+    if table_name:
+        request_data['table_name'] = table_name
+
+    if scan_type:
+        request_data['scan'] = scan_type
+
+    response = requests.post(
+        url,
+        headers={
+            'X-Metabase-Apikey': os.getenv('MB_API_KEY'),
+        },
+        json=request_data,
+    )
+
+    return response.json()
+
+
+if __name__ == '__main__':
+    fire.Fire({
+        'create_session_token': create_session_token,
+        'list_databases': list_databases,
+        'notify_rescan_db': notify_rescan_db,
+    })
